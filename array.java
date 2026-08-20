dc742025fc6304befe89f1e368c3c3bc20b6ea58
Manoj Kumar <contact@manojkumar-dev.com>
2026-08-20 23:53:06 +0200

    Update array.java

--- array.java
+++ array.java
@@ -6,3 +6,9 @@ String[] cars;
 // assign values to an "Array"
 // String "Array"
 String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+
+
+// access element
+String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+System.out.println(cars[0]);
+
