dc4602a8903fa7ded32398c7e2af2265991c544e
Manoj Kumar <contact@manojkumar-dev.com>
2026-08-20 23:54:04 +0200

    Update array.java

--- array.java
+++ array.java
@@ -8,9 +8,11 @@ String[] cars;
 String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
 
 
+
 // access element
 String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
 System.out.println(cars[0]);
+
 
 
 // change array element
@@ -19,9 +21,19 @@ cars[0] = "Tesla";
 System.out.println(cars[0]);
 
 
+
 // array length
 String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
 System.out.println(cars.length);
 
+
+// fixed-sized array using "new" keyword
+String[] cars = new String[5];
+cars[0] = "Volvo";
+cars[1] = "Tesla";
+cars[2] = "Ford";
+cars[3] = "Audi";
+cars[4] = "BMW";
+System.out.println(cars[0]);
 
 
