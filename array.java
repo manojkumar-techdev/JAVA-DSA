815c55773937681484b9cfb39c7b8dbff2bb53c3
Manoj Kumar <contact@manojkumar-dev.com>
2026-08-21 23:48:44 +0200

    Update array.java

--- array.java
+++ array.java
@@ -91,3 +91,20 @@ System.out.println("Average is :" + avg);
       lowestAge = age;
    }
 System.out.println(lowestAge);
+
+
+// using break and continue with loop
+// skip negative number and stop if value == 0
+// skip negative number and stop loop if zero found
+int[] numbers = {3, -1, 7, 0, 9};
+
+for(int n : numbers){
+   if (n < 0) {
+      continue;
+   }
+   if (n == 0) {
+      break;
+   }
+   System.out.println(n);
+}
+
