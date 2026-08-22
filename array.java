0cae63d4c5e0ed04cb35fc77328465384fdf88d3
Manoj Kumar <contact@manojkumar-dev.com>
2026-08-22 23:55:59 +0200

    Update array.java

--- array.java
+++ array.java
@@ -236,3 +236,29 @@ numbers.set(1, 99);
 
 // Print updated elements
 System.out.println("Updated second element: " + numbers.get(1));
+
+
+// update elements manually
+// Step 1: Declare and initialize the ArrayList
+ArrayList<Integer> marks = new ArrayList<>();
+marks.add(48);
+
+// Step 2: Update each element manually (split get and set)
+int firstMark = marks.get(0);
+firstMark = firstMark + 2;
+marks.set(0, firstMark);
+
+int secondMark = marks.get(1);
+secondMark = secondMark + 2;
+marks.set(1, secondMark);
+
+int thirdMark = marks.get(2);
+thirdMark = thirdMark + 2;
+marks.set(2, thirdMark);
+
+// Step 3: Print updated elements manually
+System.out.println("Updated Marks:");
+System.out.println(marks.get(0));
+System.out.println(marks.get(1));
+System.out.println(marks.get(2));
+
