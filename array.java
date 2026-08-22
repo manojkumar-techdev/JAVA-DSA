d661bbb61f2090a429306190179917f0eadd8a91
Manoj Kumar <contact@manojkumar-dev.com>
2026-08-22 23:53:30 +0200

    Update array.java

--- array.java
+++ array.java
@@ -183,3 +183,13 @@ for (int row = 0; row < myNumbers.length; row++) {
    }
 }
 
+
+// for each loop
+int[][] myNumbers = { {1, 4, 2}, {3, 6, 8, 5, 2} };
+
+for (int[] row : myNumbers) {
+   for (int num : row) {
+      System.out.println(num);
+   }
+}
+
