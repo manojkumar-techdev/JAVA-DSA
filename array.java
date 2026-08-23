df72a1a2f3ad1ad939313bf5f5af615978db9e17
Manoj Kumar <contact@manojkumar-dev.com>
2026-08-23 23:41:34 +0200

    Update array.java

--- array.java
+++ array.java
@@ -447,3 +447,20 @@ System.out.println(matrix[1][0] + " " + matrix[1][1] + " " + matrix[
 1][2]); // Print second row
 System.out.println(matrix[2][0] + " " + matrix[2][1] + " " + matrix[
 2][2]); // Print third row
+
+
+// zero matrix initialization
+Scanner scanner = new Scanner(System.in);
+// Declare a 2x2 matrix
+int[][] matrix = new int[2][2];
+// Take user input for each element
+System.out.println();
+matrix[0][0] = scanner.nextInt();
+matrix[0][1] = scanner.nextInt();
+matrix[1][0] = scanner.nextInt();
+matrix[1][1] = scanner.nextInt();
+// Display the matrix
+System.out.println("Matrix:");
+System.out.println(matrix[0][0] + " " + matrix[0][1]);
+System.out.println(matrix[1][0] + " " + matrix[1][1]);
+scanner.close();
